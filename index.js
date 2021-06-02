b3aabaf62ec3a7b7664fbcb04acca9312b6430c0
Rudy Sanchez <rudys212@gmail.com>
2021-06-02 01:59:09 -0700

    updated index.js modules

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-// TODO: Include packages needed for this application
+//external packages and dependencies needed for this application
 const inquirer = require("inquirer");
 const fs = require("fs");
+const util = require("util");
+const { getUnpackedSettings } = require("http2");
+const generateMarkd = require("./utils/generateMarkdown.js");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    message: "What is your GitHub username? (No @ before username needed)",
+    name: "username",
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
